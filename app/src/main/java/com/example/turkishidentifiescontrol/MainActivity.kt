059cd46ce6092b2cc2ec5bab59cef6695b2e15e9
Yunus Emre Kurt <yunusemre-kurt@outlook.com>
2024-03-06 17:11:31 +0300

    Create Basic UI

--- app/src/main/java/com/example/turkishidentifiescontrol/MainActivity.kt
+++ app/src/main/java/com/example/turkishidentifiescontrol/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.example.turkishidentifiescontrol
 
 import android.os.Bundle
+import android.widget.Button
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -10,5 +11,9 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.buttonControl).setOnClickListener {
+
+        }
     }
 }
